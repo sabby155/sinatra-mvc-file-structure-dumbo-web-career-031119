176bbf3ef1ce57cf9bbf0b23d9d2dd75b80152e6
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 14:37:31 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,15 @@
 class Dog 
   
+  attr_accessor :name, :age
+  attr_reader  :breed
+  
+  @@all = []
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age 
+    @@all << self
   end   
+  
   
 end   
